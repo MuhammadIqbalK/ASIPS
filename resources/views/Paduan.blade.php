b8a4bdbd82a1fb755efd8f502f4153670455aa87
MuhammadIqbalK <muhammad.iqbal.krisna-2020@vokasi.unair.ac.id>
2021-09-29 11:51:24 +0700

    commit 2

---
+++ resources/views/Paduan.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.layout')
+
+@section('content')
+
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">paduan</h1>
+    </div>
+</main>
+    
+@endsection
+
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -42,12 +42,17 @@
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
+                            <div class="sb-sidenav-menu-heading">Umum</div>
+                            <a class="nav-link" href="paduan.blade.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-file-alt"></i></div>
+                                Paduan
                             </a>
-                            <div class="sb-sidenav-menu-heading">Interface</div>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-clipboard"></i></div>
+                                Registrasi
+                            </a>
+                            
+                            <div class="sb-sidenav-menu-heading">Layanan</div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Layouts
