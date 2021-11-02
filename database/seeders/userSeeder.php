b8e3f6afdcd6f3c1776c5580ad617cc9ac9d268d
rizkyakbare <90745123+rizkyakbare@users.noreply.github.com>
2021-11-02 18:58:55 +0700

    commit5

---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => '',
+            'email' => '-2020@vokasi.unair.ac.id',
+            'password' => ''
+        ]);
+
+        User::create([
+            'name' => '',
+            'email' => '-2020@vokasi.unair.ac.id',
+            'password' => ''
+        ]);
+    }
+}
